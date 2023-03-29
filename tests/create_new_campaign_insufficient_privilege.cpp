ce225d77a0933299ab312c387e36fb623fe174f6
tosch <akirahinoshiro@gmail.com>
2023-03-29 16:49:09 +0200

    test insufficient privilege throw

---
+++ tests/create_new_campaign_insufficient_privilege.cpp
@@ -0,0 +1,28 @@
+#include "../global.hpp"
+#include "editcampaign.hpp"
+#include <filesystem>
+#include <iostream>
+
+namespace fs = std::filesystem;
+int main(int argc, char **argv)
+{
+    EditCampaign editCampaign;
+    std::string filename;
+    filename.push_back(separator);
+    filename.append("ctest_campaign");
+    filename.push_back(separator);
+    filename.append("ctest_campaign.json");
+    try
+    {
+        editCampaign.CreateNew(filename);
+    }
+    catch (std::filesystem::filesystem_error const &ex)
+    {
+        std::cout << "what():  " << ex.what() << '\n'
+                  << "path1(): " << ex.path1() << '\n'
+                  << "path2(): " << ex.path2() << '\n'
+                  << "code().value():    " << ex.code().value() << '\n'
+                  << "code().message():  " << ex.code().message() << '\n'
+                  << "code().category(): " << ex.code().category().name() << '\n';
+    }
+}
